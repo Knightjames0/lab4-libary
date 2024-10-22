5fac56040f065e42dae7c6a436b7bb246c49b651
James <79947884+Knightjames0@users.noreply.github.com>
2024-10-22 16:50:04 -0700

    Changed Book

--- src/lab4/library/Book.java
+++ src/lab4/library/Book.java
@@ -4,9 +4,27 @@ public class Book {
 
 	// GRASP Principle: Information Expert
 	// Manages its own state (available or borrowed).
-	private String title;
-	private boolean isAvailable; 
+	private String _title;
+	private boolean _isAvailable; 
 	
 	// TODO: implement Book functionality 
+	public Book(String title){
+		_title = title;
+		_isAvailable = true;
+	}
+
+	public boolean isAvailable(){
+		return _isAvailable;
+	}
+
+	public void setIsAvailable(Boolean state){
+		_isAvailable = state;
+	}
+	public String getTitle(){
+		return _title;
+	}
+	public String toString(){
+		return _title;
+	}
 	
 }
